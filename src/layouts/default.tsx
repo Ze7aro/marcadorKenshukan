35a9e68582ed83d2f5e6daf411dfc97543c19359
Zetaro <jasynto.zero@gmail.com>
2025-06-23 18:32:24 -0300

    [WIP] correcciones de build

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,3 @@
-import { Image } from "@heroui/react";
-import Logo from "@/assets/images/Logo-2.png";
-
 export default function DefaultLayout({
   children,
 }: {
@@ -10,11 +7,6 @@ export default function DefaultLayout({
     <div className="relative flex flex-col h-screen">
       <main>{children}</main>
       <footer className="w-full h-8 flex gap-4 absolute z-[9999] bottom-0 items-center justify-start px-10">
-        {/*  <span className="bg-gradient-to-r from-white via-gray-300 to-blue-500 bg-clip-text text-transparent font-semibold italic">
-          Designed by
-        </span>
-        <Image alt="Logo" height={30} src={Logo} width={30} />
- */}{" "}
       </footer>
     </div>
   );
